d4bb450cb52e4bae765dc53c1ef5c636eda0691a
Zoltan Toth <z.toth@tricentis.com>
2020-06-25 15:09:42 +0200

    Check for "unicode normalisation" (simplified StringNormalizer), example green

---
+++ UserRepository/StringNormalizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRepository
+{
+    public class StringNormalizer
+    {
+        public string Normalize(string s)
+        {
+            return NormalizeUnicode(s)?.ToLowerInvariant();
+        }
+
+        //TODO: implement a proper stringprep implementation according to https://tools.ietf.org/html/rfc3454
+        private string NormalizeUnicode(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return null;
+
+            var builder = new StringBuilder();
+
+            foreach (var c in s.Normalize(NormalizationForm.FormC))
+            {
+                builder.Append(MapUnicodeChar(c));
+            }
+
+            return builder.ToString();
+        }
+
+        private static readonly Dictionary<char, char> CharMap = new Dictionary<char, char>
+        {
+            {'s', 's'},
+            {'ᴛ', 't'},
+            {'ᴇ', 'e'},
+            {'ᴠ', 'v'},
+            {'ᴵ', 'i'},
+        };
+
+        private char MapUnicodeChar(in char c)
+        {
+            if (CharMap.TryGetValue(c, out var mapped))
+                return mapped;
+
+            return c;
+        }
+    }
+}
--- UserRepository/UserRepository.cs
+++ UserRepository/UserRepository.cs
@@ -6,10 +6,12 @@ namespace UserRepository
     public class UserRepository
     {
         private readonly UserRepositoryStore _userRepositoryStore;
+        private readonly StringNormalizer _stringNormalizer;
 
-        public UserRepository(UserRepositoryStore userRepositoryStore)
+        public UserRepository(UserRepositoryStore userRepositoryStore, StringNormalizer stringNormalizer)
         {
             _userRepositoryStore = userRepositoryStore;
+            _stringNormalizer = stringNormalizer;
         }
 
         public void RegisterUser(string personalName, string userName, string email)
@@ -28,9 +30,16 @@ namespace UserRepository
 
         private void CheckForIdenticalUserName(string userName)
         {
+            var normalizedUserName = NormalizeUserName(userName);
+
             if (_userRepositoryStore.GetUsers()
-                .Any(u => string.Equals(u.UserName, userName, StringComparison.InvariantCultureIgnoreCase)))
+                .Any(u => string.Equals(NormalizeUserName(u.UserName), normalizedUserName, StringComparison.InvariantCultureIgnoreCase)))
                 throw new UserRepositoryException("Username taken");
+        }
+
+        private string NormalizeUserName(string userName)
+        {
+            return _stringNormalizer.Normalize(userName);
         }
     }
 }
